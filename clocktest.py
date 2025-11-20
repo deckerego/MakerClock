95dc77502b6abbfb77f09977cfd207743d2a444b
John Ellis <532789+deckerego@users.noreply.github.com>
2025-11-20 12:32:37 -0500

    Running clock test

--- clocktest.py
+++ clocktest.py
@@ -1,11 +1,16 @@
+import time
+import datetime
 import board
 import busio
 from adafruit_ht16k33 import segments
+
 i2c = busio.I2C(board.SCL, board.SDA)
 display = segments.BigSeg7x4(i2c)
 display.brightness = 0.5
 display.fill(False)
-display.print("88:88")
-display.top_left_dot = True
-display.bottom_left_dot = True
-display.ampm = True
+
+while True:
+    now = datetime.datetime.now()
+    display.print(f"{now.hour:02}:{now.minute:02}")
+    display.ampm = now.hour >= 12
+    time.sleep(15)
